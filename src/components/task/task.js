1de2b198144e48324d1dfaa7b8262cf67f2ab77e
Janet Woods <janetnwoods@gmail.com>
2019-03-06 13:09:48 -0600

    getting new navandroute

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ApplicationViews from "../ApplicationViews"
+import taskManager from ".../taskManager"
 
 export default class TaskAddForm extends Component {
 
@@ -13,23 +14,36 @@ export default class TaskAddForm extends Component {
         const stateToChange = {};
         stateToChange[evt.target.id] = evt.target.value;
         this.setState(stateToChange);
-      };
+      }
 
       makeNewTask = evt => {
           evt.preventDefault();
 
-          const newTask ={
-            userId = this.state.userId,
+          if (this.state.taskName === "") {
+            window.alert("Please enter a task");
+          } else if(this.state.dateToComplete === ""){
+              window.alert("enter an expected completion date")
+          }else
+          {
+          const newTask = {
+            userId: parseInt(this.state.userId),
             taskName: this.state.taskName,
-            dateToComplete = this.state.dateToComplete,
-            completed = false
+            dateToComplete: this.state.dateToComplete,
+            completed: false
           }
-      }
+        this.props.taskManager.addTask(newTask)
+        .then(()=>this.props.history.push("./taskList")))
+    }
+
 
       render(){
           return(
             <React.Fragment>
+                <form class="form-new-task">
+                    <div className="form-group">
 
+                    </div>
+                </form>
 
             </React.Fragment>
           )
